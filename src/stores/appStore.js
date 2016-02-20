32a70f257e1546fa77a798e8919e67b87af75d95
Js41637 ftw <js41637@gmail.com>
2016-02-20 18:51:35 +1100

    .

--- src/stores/appStore.js
+++ src/stores/appStore.js
@@ -11,7 +11,7 @@ class AppStore {
 	}
 	onSetProgress(progress, eta) {
         this.setState({
-            progress: eta,
+            progress: progress,
             eta: eta
         });
     }
